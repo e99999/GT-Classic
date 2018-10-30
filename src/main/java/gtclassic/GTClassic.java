2800545cac6e3ad97a22ca21a3564a10f317378c
e99999 <26906600+e99999@users.noreply.github.com>
2018-10-30 16:18:11 -0700

    resources and setting up

--- src/main/java/gtclassic/GTClassic.java
+++ src/main/java/gtclassic/GTClassic.java
@@ -1,17 +1,16 @@
-package com.example.examplemod;
+package gtclassic;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = GTClassic.MODID, name = GTClassic.NAME, version = GTClassic.VERSION,  dependencies = "required-after:forge@[14.23.5.2772,)", useMetadata = true)
+public class GTClassic
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "gtclassic";
+    public static final String NAME = "Gregtech Classic";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -25,7 +24,7 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        // some hello world action
+        logger.info("HELLO WORLD!");
     }
 }
