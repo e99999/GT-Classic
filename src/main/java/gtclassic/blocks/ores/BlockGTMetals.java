1c2fdb315983440c2f3662f024604a85bdaa926e
Trinsdar <westbound1715@gmail.com>
2018-11-03 16:36:47 -0400

    Added Metal Blocks Class

---
+++ src/main/java/gtclassic/blocks/ores/BlockGTMetals.java
@@ -0,0 +1,36 @@
+package gtclassic.blocks.ores;
+
+import gtclassic.GTItems;
+import gtclassic.GTMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockGTMetals extends Block {
+    public enum BlockGTMetalsVariants{
+        RUBY, SAPPHIRE, ALUMINUM, TITANIUM, CHROME, STEEL, BRASS, LEAD, ELECTRUM, ZINC, OLIVINE, GREEN_SAPPHIRE, PLATINUM, TUNGSTEN, NICKEL, TUNGSTENSTEEL, IRIDIUM_REINFORCED_TUNGSTENSTEEL, INVAR, OSMIUM, IRIDIUM
+    }
+
+    BlockGTMetalsVariants variant;
+    public BlockGTMetals(BlockGTMetalsVariants variant){
+        super(Material.ROCK);
+        this.variant = variant;
+        setRegistryName(variant.toString().toLowerCase() + "_block");
+        setUnlocalizedName(GTMod.MODID + "." + variant.toString().toLowerCase() + "_block");
+        setCreativeTab(GTItems.tabGTClassic);
+        setHardness(5.0F);
+        setSoundType(SoundType.STONE);
+        setHarvestLevel("pickaxe", 1);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+
+    }
+}
