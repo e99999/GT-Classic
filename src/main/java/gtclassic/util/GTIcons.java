fde3b38ac7364bea1817e79388b0f320a3d6746d
Trinsdar <westbound1715@gmail.com>
2018-11-05 12:47:12 -0500

    added components

--- src/main/java/gtclassic/util/GTIcons.java
+++ src/main/java/gtclassic/util/GTIcons.java
@@ -16,11 +16,13 @@ public class GTIcons {
         addSprite(new Sprites.SpriteData("gtclassic_duststiny", "gtclassic:textures/sprites/duststiny.png", new Sprites.SpriteInfo(16, 16)));
         addSprite(new Sprites.SpriteData("gtclassic_nuggets", "gtclassic:textures/sprites/nuggets.png", new Sprites.SpriteInfo(16, 16)));
         addSprite(new Sprites.SpriteData("gtclassic_materials", "gtclassic:textures/sprites/materials.png", new Sprites.SpriteInfo(16, 16)));
+        addSprite(new Sprites.SpriteData("gtclassic_components", "gtclassic:textures/sprites/components.png", new Sprites.SpriteInfo(16, 16)));
         addTextureEntry(new Sprites.TextureEntry("gtclassic_blocks", 0, 0, 16, 2));
         addTextureEntry(new Sprites.TextureEntry("gtclassic_blocks", 0, 6, 12, 7));
         addTextureEntry(new Sprites.TextureEntry("gtclassic_dusts", 0, 0, 16, 4));
         addTextureEntry(new Sprites.TextureEntry("gtclassic_duststiny", 0, 0, 16, 6));
         addTextureEntry(new Sprites.TextureEntry("gtclassic_nuggets", 0, 0, 16, 1));
         addTextureEntry(new Sprites.TextureEntry("gtclassic_materials", 0, 0, 16, 4));
+        addTextureEntry(new Sprites.TextureEntry("gtclassic_components", 0, 0, 16, 3));
     }
 }
