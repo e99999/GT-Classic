027a1fa5f88825025149bd5fb9723550c19cbd7f
Trinsdar <westbound1715@gmail.com>
2018-11-05 21:21:24 -0500

    added Advanced Drill class

---
+++ src/main/java/gtclassic/items/tools/GTItemAdvancedDrill.java
@@ -0,0 +1,9 @@
+package gtclassic.items.tools;
+
+import ic2.core.item.tool.electric.ItemElectricToolDrill;
+
+public class GTItemAdvancedDrill extends ItemElectricToolDrill {
+    public GTItemAdvancedDrill(){
+
+    }
+}
