dbc45db5451f93123ef7da1764131163473a14ea
Trinsdar <westbound1715@gmail.com>
2018-11-06 23:28:46 -0500

    added lithium battery and lapotronic energy orb

---
+++ src/main/java/gtclassic/items/eustorage/GTItemLapotronicEnergyOrb.java
@@ -0,0 +1,53 @@
+package gtclassic.items.eustorage;
+
+import gtclassic.GTClassic;
+import ic2.api.item.ElectricItem;
+import ic2.core.item.base.ItemBatteryBase;
+import ic2.core.platform.registry.Ic2Sounds;
+import ic2.core.platform.textures.Ic2Icons;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class GTItemLapotronicEnergyOrb extends ItemBatteryBase {
+    public GTItemLapotronicEnergyOrb() {
+        super(0);
+        this.setRightClick();
+        this.setRegistryName("lapotronic_energy_orb");
+        this.setUnlocalizedName(GTClassic.MODID + ".lapotronicEnergyOrb");
+        this.maxCharge = 10000000;
+        this.transferLimit = 1000;
+        this.tier = 4;
+        this.provider = true;
+        this.setCreativeTab(GTClassic.creativeTabGT);
+    }
+
+    public int getItemStackLimit(ItemStack stack) {
+        return 1;
+    }
+
+    public boolean isDamaged(ItemStack stack) {
+        return true;
+    }
+
+
+    public boolean showDurabilityBar(ItemStack stack) {
+        return true;
+    }
+
+    public boolean wantsToPlay(ItemStack stack) {
+        return true;
+    }
+
+    public ResourceLocation createSound(ItemStack stack) {
+        return Ic2Sounds.batteryUse;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public TextureAtlasSprite getTexture(ItemStack item) {
+        return Ic2Icons.getTextures("gtclassic_items")[37];
+    }
+}
