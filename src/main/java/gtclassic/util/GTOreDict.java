503f0ea2a2a0fcd3d807d0db0d0ea49b03ba6839
e99999 <26906600+e99999@users.noreply.github.com>
2018-11-17 17:39:38 -0800

    finishing ore dict & uploading GUI textures

--- src/main/java/gtclassic/util/GTOreDict.java
+++ src/main/java/gtclassic/util/GTOreDict.java
@@ -5,15 +5,44 @@ import net.minecraftforge.oredict.OreDictionary;
 public class GTOreDict {
 	
 	public static void init () {
+		OreDictionary.registerOre("dustEnderPearl", GTItems.dustEnderpearl);
+		OreDictionary.registerOre("dustEnderEye", GTItems.dustEnderEye);
+		OreDictionary.registerOre("gemLapis", GTItems.dustLazurite);
+	    OreDictionary.registerOre("dustLazurite", GTItems.dustLazurite);
+	    OreDictionary.registerOre("dustPyrite", GTItems.dustPyrite);
+	    OreDictionary.registerOre("dustCalcite", GTItems.dustCalcite);
+	    OreDictionary.registerOre("dustNetherrack", GTItems.dustNetherrack);
+	    OreDictionary.registerOre("dustFlint", GTItems.dustFlint);
+	    OreDictionary.registerOre("dustUranium", GTItems.dustUranium);
+	    OreDictionary.registerOre("dustBauxite", GTItems.dustBauxite);
+	    OreDictionary.registerOre("dustAluminum", GTItems.dustAluminum);
+	    OreDictionary.registerOre("dustTitanium", GTItems.dustTitanium);
+	    OreDictionary.registerOre("dustChrome", GTItems.dustChrome);
 		OreDictionary.registerOre("dustRuby", GTItems.dustRuby);
-        OreDictionary.registerOre("gemRuby", GTItems.ruby);
-        OreDictionary.registerOre("blockRuby", GTBlocks.rubyBlock);
+		OreDictionary.registerOre("dustSapphire", GTItems.dustSapphire);
+		OreDictionary.registerOre("dustGreenSapphire", GTItems.dustGreenSapphire);
+		OreDictionary.registerOre("dustEmerald", GTItems.dustEmerald);
+		
         OreDictionary.registerOre("oreRuby", GTBlocks.rubyOre);
-        
-        OreDictionary.registerOre("dustSapphire", GTItems.dustSapphire);
-        OreDictionary.registerOre("gemSapphire", GTItems.sapphire);
-        OreDictionary.registerOre("blockSapphire", GTBlocks.sapphireBlock);
         OreDictionary.registerOre("oreSapphire", GTBlocks.sapphireOre);
+        OreDictionary.registerOre("oreIridium", GTBlocks.iridiumOre);
+        OreDictionary.registerOre("oreBauxite", GTBlocks.bauxiteOre);
+        OreDictionary.registerOre("oreMagnetite", GTBlocks.sandIron);
+        
+        OreDictionary.registerOre("gemRuby", GTItems.ruby);
+        OreDictionary.registerOre("gemSapphire", GTItems.sapphire);
+        OreDictionary.registerOre("ingotAluminum", GTItems.ingotAluminum);
+        OreDictionary.registerOre("ingotChrome", GTItems.ingotChrome);
+        OreDictionary.registerOre("ingotIridium", GTItems.ingotIridium);
+        OreDictionary.registerOre("ingotTitanium", GTItems.ingotTitanium);
+        
+        OreDictionary.registerOre("blockRuby", GTBlocks.rubyBlock);
+        OreDictionary.registerOre("blockSapphire", GTBlocks.sapphireBlock);
+        OreDictionary.registerOre("blockAluminum", GTBlocks.aluminumBlock);
+        OreDictionary.registerOre("blockChrome", GTBlocks.chromeBlock);
+        OreDictionary.registerOre("blockIridium", GTBlocks.iridiumBlock);
+        OreDictionary.registerOre("blockTitanium", GTBlocks.titaniumBlock);
+        
 	}
 
 }
--- src/main/java/gtclassic/util/GTRecipes.java
+++ src/main/java/gtclassic/util/GTRecipes.java
@@ -1,15 +1,11 @@
 package gtclassic.util;
 
-import gtclassic.blocks.resources.GTBlockMetal;
-import gtclassic.items.resources.GTItemGem;
-import gtclassic.items.resources.GTItemIngot;
 import ic2.api.classic.recipe.ClassicRecipes;
 import ic2.api.classic.recipe.crafting.ICraftingRecipeList;
 import ic2.core.block.machine.low.TileEntityCompressor;
 import ic2.core.block.machine.low.TileEntityMacerator;
 import ic2.core.platform.registry.Ic2Items;
 import ic2.core.util.misc.StackUtil;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -31,19 +27,6 @@ public class GTRecipes {
 	static ICraftingRecipeList recipes = ClassicRecipes.advCrafting;
 	
 	public static void initShapelessRecipes () {
-		
-		recipes.addRecipe(new ItemStack(GTBlocks.rubyBlock, 1),
-				new Object[]{"III", "III", "III", 'I', "gemRuby"});
-		
-		recipes.addShapelessRecipe(new ItemStack(GTItems.ruby, 9),
-				new Object[]{GTBlocks.rubyBlock});
-		
-		recipes.addRecipe(new ItemStack(GTBlocks.sapphireBlock, 1),
-				new Object[]{"III", "III", "III", 'I', "gemSapphire"});
-		
-		recipes.addShapelessRecipe(new ItemStack(GTItems.sapphire, 9),
-				new Object[]{GTBlocks.sapphireBlock});
-		
 	}
 		
 	public static void initShapedRecipes () {
@@ -57,13 +40,22 @@ public class GTRecipes {
 		recipes.addRecipe(Ic2Items.lapotronCrystal.copy(), 
 				new Object[]{"LCL", "LDL", "LCL", 'D', "gemSapphire", 'C', Ic2Items.electricCircuit.copy(), 'L', "gemLapis"});
 		
+		
 	}
 	
 	public static void initMachineRecipes () {
 		TileEntityCompressor.addRecipe("dustRuby", 1, new ItemStack(GTItems.ruby), 0.1F);
+		TileEntityCompressor.addRecipe("gemRuby", 9, new ItemStack(GTBlocks.rubyBlock), 0.1F);
 		TileEntityCompressor.addRecipe("dustSapphire", 1, new ItemStack(GTItems.sapphire), 0.1F);
-		TileEntityMacerator.addRecipe("oreRuby", 0, StackUtil.copyWithSize(new ItemStack(GTItems.ruby), 2), 0.3F);
-		TileEntityMacerator.addRecipe("oreSapphire", 0, StackUtil.copyWithSize(new ItemStack(GTItems.sapphire), 2), 0.3F);
+		TileEntityCompressor.addRecipe("gemSapphire", 9, new ItemStack(GTBlocks.sapphireBlock), 0.1F);
+		
+		TileEntityMacerator.addRecipe("oreRuby", 1, StackUtil.copyWithSize(new ItemStack(GTItems.ruby), 2), 0.3F);
+		TileEntityMacerator.addRecipe("blockRuby", 1, StackUtil.copyWithSize(new ItemStack(GTItems.dustRuby), 9), 0.1F);
+		TileEntityMacerator.addRecipe("oreSapphire", 1, StackUtil.copyWithSize(new ItemStack(GTItems.sapphire), 2), 0.3F);
+		TileEntityMacerator.addRecipe("blockSapphire", 1, StackUtil.copyWithSize(new ItemStack(GTItems.dustSapphire), 9), 0.1F);
+		
+		TileEntityMacerator.addRecipe("oreBauxite", 1, StackUtil.copyWithSize(new ItemStack(GTItems.dustBauxite), 4), 0.1F);
+		TileEntityMacerator.addRecipe("oreIridium", 1, StackUtil.copyWithSize(Ic2Items.iridiumOre, 2), 1.0F);
 		
 		
 	}
