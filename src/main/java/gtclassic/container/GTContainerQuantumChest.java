032804135cc1ed53eba331ebbc344b1c9be0435e
e99999 <26906600+e99999@users.noreply.github.com>
2018-11-28 18:31:25 -0800

    update Qchest slots

--- src/main/java/gtclassic/container/GTContainerQuantumChest.java
+++ src/main/java/gtclassic/container/GTContainerQuantumChest.java
@@ -5,23 +5,27 @@ import gtclassic.tileentity.GTTileEntityQuantumChest;
 import ic2.core.inventory.container.ContainerTileComponent;
 import ic2.core.inventory.slots.SlotBase;
 import ic2.core.inventory.slots.SlotDisplay;
+import ic2.core.inventory.slots.SlotOutput;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
 
 public class GTContainerQuantumChest extends ContainerTileComponent<GTTileEntityQuantumChest> {
+	
 	public static ResourceLocation TEXTURE = new ResourceLocation(GTClassic.MODID, "textures/gui/quantumchest.png");
 	
-	public GTContainerQuantumChest(InventoryPlayer player, GTTileEntityQuantumChest tile) {
+	public GTContainerQuantumChest(InventoryPlayer player, GTTileEntityQuantumChest tile) 
+	{
 		super(tile);
 		this.addSlotToContainer(new SlotBase(tile, 0, 80, 17));
-		this.addSlotToContainer(new SlotBase(tile, 1, 80, 53));
+		this.addSlotToContainer(new SlotOutput(player.player, tile, 1, 80, 53));
 		this.addSlotToContainer(new SlotDisplay(tile, 2, 59, 42));
 		this.addPlayerInventory(player, 0, 0);
 	}
 	
 	@Override
-	public ResourceLocation getTexture() {
+	public ResourceLocation getTexture() 
+	{
 		return TEXTURE;
 	}
 
@@ -32,7 +36,8 @@ public class GTContainerQuantumChest extends ContainerTileComponent<GTTileEntity
     }
 
 	@Override
-	public int guiInventorySize() {
+	public int guiInventorySize() 
+	{
 		return 3;
 	}
 	
