6ea8a05ec00b67d13e97f6a7595d19c0e81205e8
Trinsdar <westbound1715@gmail.com>
2018-11-29 18:50:08 -0500

    implemented basic in to out tooltip. does not obey the eu reader yet and is probably not acurate in packets emmitted

--- src/main/java/gtclassic/blocks/GTBlockEnergy.java
+++ src/main/java/gtclassic/blocks/GTBlockEnergy.java
@@ -11,6 +11,9 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
@@ -68,6 +71,16 @@ public class GTBlockEnergy extends BlockMultiID {
             return new GTTileEntitySuperCondensator();
         }else{
             return new TileEntityBlock();
+        }
+
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        if (this == GTBlocks.superCondensator)
+        {
+            tooltip.add(I18n.format("tooltip."+ GTClassic.MODID +".superCondensator"));
         }
 
     }
