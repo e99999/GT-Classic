f0d816c2dbe188f9911cedbdd9340dfa7dc42c3a
e99999 <26906600+e99999@users.noreply.github.com>
2018-12-01 11:39:38 -0800

    crafting tablet now crafts

--- src/main/java/gtclassic/container/GTContainerCraftingTablet.java
+++ src/main/java/gtclassic/container/GTContainerCraftingTablet.java
@@ -11,27 +11,69 @@ import ic2.core.inventory.slots.SlotCustom;
 import ic2.core.inventory.slots.SlotOutput;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryCraftResult;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.inventory.Slot;
+import net.minecraft.inventory.SlotCrafting;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class GTContainerCraftingTablet extends ContainerItemComponent<GTInventoryCraftingTablet> {
 	
 	public static ResourceLocation TEXTURE = new ResourceLocation(GTClassic.MODID, "textures/gui/craftingtablet.png");
 	
+	public InventoryCrafting craftMatrix = new InventoryCrafting(this, 3, 3);
+    public InventoryCraftResult craftResult = new InventoryCraftResult();
+
+    private final World world;
+    private final BlockPos pos;
+    private final EntityPlayer player;
+	
 	public GTContainerCraftingTablet(GTInventoryCraftingTablet inv, int id, InventoryPlayer player) 
 	{
 		super(inv, id);
-		this.addSlotToContainer(new SlotCustom(inv, 0, 30, 17, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotCustom(inv, 1, 48, 17, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotCustom(inv, 2, 66, 17, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotCustom(inv, 3, 30, 35, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotCustom(inv, 4, 48, 35, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotCustom(inv, 5, 66, 35, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotCustom(inv, 6, 30, 53, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotCustom(inv, 7, 48, 53, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotCustom(inv, 8, 66, 53, new InvertedFilter(new BasicItemFilter(GTItems.craftingTablet))));
-		this.addSlotToContainer(new SlotOutput(player.player,inv, 9, 124, 35));
+		this.world = player.player.getEntityWorld();
+        this.pos = player.player.getPosition();
+        this.player = player.player;
+        this.addSlotToContainer(new SlotCrafting(this.player, this.craftMatrix, this.craftResult, 0, 124, 35));
+
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                this.addSlotToContainer(new Slot(this.craftMatrix, j + i * 3, 30 + j * 18, 17 + i * 18));
+            }
+        }
+
+        for (int k = 0; k < 3; ++k)
+        {
+            for (int i1 = 0; i1 < 9; ++i1)
+            {
+                this.addSlotToContainer(new Slot(player, i1 + k * 9 + 9, 8 + i1 * 18, 84 + k * 18));
+            }
+        }
+
 		this.addPlayerInventory(player, 0, 0);
 	}
+	
+	@Override
+	public void onCraftMatrixChanged(IInventory inventoryIn)
+    {
+        this.slotChangedCraftingGrid(this.world, this.player, this.craftMatrix, this.craftResult);
+    }
+	
+	@Override
+	public void onContainerClosed(EntityPlayer playerIn)
+    {
+        super.onContainerClosed(playerIn);
+
+        if (!this.world.isRemote)
+        {
+            this.clearContainer(playerIn, this.world, this.craftMatrix);
+        }
+    }
 	
 	@Override
 	public ResourceLocation getTexture() {
