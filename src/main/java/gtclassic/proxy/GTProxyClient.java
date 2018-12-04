3937600714aec644c0443b0e4382ab353f317bf0
e99999 <26906600+e99999@users.noreply.github.com>
2018-12-04 12:59:25 -0800

    broken textures refactor

--- src/main/java/gtclassic/proxy/GTProxyClient.java
+++ src/main/java/gtclassic/proxy/GTProxyClient.java
@@ -1,8 +1,8 @@
 package gtclassic.proxy;
 
 import gtclassic.util.GTBlocks;
-import gtclassic.util.GTItems;
-
+import gtclassic.util.GTIcons;
+import ic2.core.platform.textures.Ic2Icons.SpriteReloadEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -11,13 +11,21 @@ import net.minecraftforge.fml.relauncher.Side;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class GTProxyClient extends GTProxyCommon {
-    @Override
-    public void preInit(FMLPreInitializationEvent e) {
-        super.preInit(e);
+    
+	@Override
+    public void preInit(FMLPreInitializationEvent e) 
+	{
+    	super.preInit(e);
     }
 
+	public void onIconLoad(SpriteReloadEvent event)
+	{
+	    GTIcons.loadSprites();
+	}
+    
     @SubscribeEvent
-    public static void registerModels(ModelRegistryEvent event) {
+    public static void registerModels(ModelRegistryEvent event) 
+    {
     	GTBlocks.initModels();
     }
 }
