e2837b2e0682f8f9e093a7bf8bd44ef86d710499
e99999 <26906600+e99999@users.noreply.github.com>
2018-12-10 21:32:24 -0800

    lightning rod stuff

--- src/main/java/gtclassic/tileentity/GTTileEntityLightningRod.java
+++ src/main/java/gtclassic/tileentity/GTTileEntityLightningRod.java
@@ -2,16 +2,23 @@ package gtclassic.tileentity;
 
 import java.util.Random;
 
+import gtclassic.util.GTBlocks;
 import ic2.core.block.base.tile.TileEntityGeneratorBase;
 import ic2.core.inventory.container.ContainerIC2;
+import ic2.core.platform.registry.Ic2Items;
+import ic2.core.platform.registry.Ic2States;
 import ic2.core.util.math.Box2D;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class GTTileEntityLightningRod extends TileEntityGeneratorBase {
+
+	public static final IBlockState rodState = Ic2States.ironFence;
 
 	public GTTileEntityLightningRod() {
 		super(0);
@@ -36,13 +43,15 @@ public class GTTileEntityLightningRod extends TileEntityGeneratorBase {
 	public void update() {
 		Random rand = world instanceof World ? world.rand : new Random();
 
-		if (world.getTotalWorldTime() % 256 == 0 && correctWeather(this.world, this.getPos().up())
-				&& (rand.nextInt(10) == 0)) {
-			this.world.addWeatherEffect(new EntityLightningBolt(this.world, this.getPos().getX(), this.getPos().getY(),
-					this.getPos().getZ(), false));
-			if (this.storage < this.maxStorage) {
+		if (world.getTotalWorldTime() % 256 == 0 /* && correctWeather(this.world, this.getPos().up()) */
+				&& rand.nextInt(10) == 0) {
+			if (checkStructure()) {
+				this.world.addWeatherEffect(new EntityLightningBolt(this.world, this.getPos().getX(),
+						this.getPos().getY(), this.getPos().getZ(), false));
+				if (this.storage < this.maxStorage) {
 
-				this.storage = Math.min(this.maxStorage, this.storage + (production));
+					this.storage = Math.min(this.maxStorage, this.storage + (production));
+				}
 			}
 		}
 
@@ -71,6 +80,58 @@ public class GTTileEntityLightningRod extends TileEntityGeneratorBase {
 	@Override
 	public boolean hasGui(EntityPlayer player) {
 		return false;
+	}
+
+	public boolean checkStructure() {
+		// if (world.isRemote) return false; //Return false if on client side
+		if (!world.isAreaLoaded(pos, 3))
+			return false;
+
+		BlockPos working;
+
+		// vertical line upward
+		working = pos.offset(EnumFacing.UP, 1);
+		if (!(checkPos(working))) {
+			return false;
+		}
+		working = pos.offset(EnumFacing.UP, 2);
+		if (!(checkPos(working))) {
+			return false;
+		}
+		working = pos.offset(EnumFacing.UP, 3);
+		if (!(checkPos(working))) {
+			return false;
+		}
+		working = pos.offset(EnumFacing.UP, 4);
+		if (!(checkPos(working))) {
+			return false;
+		}
+		working = pos.offset(EnumFacing.UP, 5);
+		if (!(checkPos(working))) {
+			return false;
+		}
+		working = pos.offset(EnumFacing.UP, 6);
+		if (!(checkPos(working))) {
+			return false;
+		}
+		working = pos.offset(EnumFacing.UP, 7);
+		if (!(checkPos(working))) {
+			return false;
+		}
+		working = pos.offset(EnumFacing.UP, 8);
+		if (!(checkPos(working))) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean checkPos(BlockPos pos) {
+		return world.getBlockState(pos) == rodState;
+	}
+
+	public boolean checkPos(BlockPos pos, EnumFacing facing, int offset) {
+		return checkPos(pos.offset(facing, offset));
 	}
 
 }
