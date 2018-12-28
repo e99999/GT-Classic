1432e6b33a858b0ab20bdeeb6435036a2f9b97e2
Speiger <moritzstehr@gmx.de>
2018-12-29 00:25:18 +0100

    Backwards compat to older forge versions.

--- src/main/java/gtclassic/proxy/GTProxyClient.java
+++ src/main/java/gtclassic/proxy/GTProxyClient.java
@@ -2,21 +2,20 @@ package gtclassic.proxy;
 
 import gtclassic.GTIcons;
 import ic2.core.platform.textures.Ic2Icons.SpriteReloadEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
-@Mod.EventBusSubscriber(Side.CLIENT)
 public class GTProxyClient extends GTProxyCommon {
 
 	@Override
 	public void preInit(FMLPreInitializationEvent e) {
 		super.preInit(e);
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@SubscribeEvent
-	public static void onIconLoad(SpriteReloadEvent event) {
+	public void onIconLoad(SpriteReloadEvent event) {
 		GTIcons.loadSprites();
 	}
 }
