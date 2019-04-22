37f2d04a19baed838a8ac97d245f6db51f9818a3
Meduris <Meduris@users.noreply.github.com>
2019-04-22 17:42:58 +0200

    GTSluiceBoxModel & GTSluiceBoxExtensionModel: - improve model caching (one static instance which is automatically reseted on texture reload)
    GTTileFacing:
    - allow restricting allowed rotations
    - allow wrench to break the block
    
    GTBlockSluice & GTBlockSluiceBoxExt:
    - use GTTileFacing with only horizontal rotations

--- src/main/java/gtclassic/block/GTBlockSluiceBoxExt.java
+++ src/main/java/gtclassic/block/GTBlockSluiceBoxExt.java
@@ -2,13 +2,17 @@ package gtclassic.block;
 
 import gtclassic.GTMod;
 import gtclassic.models.GTSluiceBoxExtensionModel;
+import gtclassic.tile.GTTileFacing;
 import gtclassic.util.GTValues;
 import ic2.core.platform.textures.models.BaseModel;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -26,7 +30,12 @@ public class GTBlockSluiceBoxExt extends GTBlockFacing {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public BaseModel getModelFromState(IBlockState state) {
-		return new GTSluiceBoxExtensionModel(getRotation(state));
+		return GTSluiceBoxExtensionModel.getModel();
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World worldIn, int meta) {
+		return new GTTileFacing(EnumFacing.HORIZONTALS);
 	}
 
 	@Override
