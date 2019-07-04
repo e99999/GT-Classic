67ada1b815cbfc4984a5f1985c02b6ee494ce69f
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-07-03 23:16:28 -0400

    made GTMaterialFlag a class

--- src/main/java/gtclassic/material/GTMaterialFlag.java
+++ src/main/java/gtclassic/material/GTMaterialFlag.java
@@ -1,21 +1,25 @@
 package gtclassic.material;
 
-public enum GTMaterialFlag {
-	DUST("_dust", 0, false),
-	INGOT("_ingot", 1, false),
-	RUBY("_gem", 2, false),
-	SAPPHIRE("_gem", 3, false),
-	FLUID("", 13, true),
-	GAS("", 13, true),
-	BLOCK("_block", 16, false);
+public class GTMaterialFlag {
 
+	public static GTMaterialFlag
+	DUST = new GTMaterialFlag("_dust", 0, false),
+	INGOT = new GTMaterialFlag("_ingot", 1, false),
+	RUBY = new GTMaterialFlag("_gem", 2, false),
+	SAPPHIRE = new GTMaterialFlag("_gem", 3, false),
+	FLUID = new GTMaterialFlag("", 13, true),
+	GAS = new GTMaterialFlag("", 13, true),
+	BLOCK = new GTMaterialFlag("_block", 16, false);
+
+
+	private static int LAST_INTERNAL_ID;
 	private int mask;
 	private String suffix;
 	private int id;
 	private boolean layered;
 
-	GTMaterialFlag(String suffix, int id, boolean layered) {
-		this.mask = 1 << ordinal();
+	public GTMaterialFlag(String suffix, int id, boolean layered) {
+		this.mask = 1 << LAST_INTERNAL_ID++;
 		this.suffix = suffix;
 		this.id = id;
 		this.layered = layered;
