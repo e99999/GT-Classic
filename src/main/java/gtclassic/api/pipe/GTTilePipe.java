cfad5e3fc510c432dd1ab77609849f32dac20796
e99999 <26906600+e99999@users.noreply.github.com>
2019-12-04 01:59:37 -0800

    working on pipe tile

--- src/main/java/gtclassic/api/pipe/GTTilePipe.java
+++ src/main/java/gtclassic/api/pipe/GTTilePipe.java
@@ -1,15 +1,74 @@
 package gtclassic.api.pipe;
 
+import ic2.api.classic.network.adv.NetworkField;
+import ic2.core.RotationList;
 import ic2.core.block.base.tile.TileEntityBlock;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 
-public class GTTilePipe extends TileEntityBlock{
-	
+public class GTTilePipe extends TileEntityBlock {
+
+	@NetworkField(index = 8)
+	public RotationList connection;
+
 	public GTTilePipe() {
-		
+		this.connection = RotationList.EMPTY;
+		this.addNetworkFields(new String[] { "connection" });
 	}
-	
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt) {
+		super.readFromNBT(nbt);
+		this.connection = RotationList.ofNumber(nbt.getByte("Connection"));
+	}
+
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
+		super.writeToNBT(nbt);
+		nbt.setByte("Connection", (byte) this.connection.getCode());
+		return nbt;
+	}
+
 	public Vec3i getConnections() {
-	      return new Vec3i(1, 2, 3);
-	   }
+		return new Vec3i(this.connection.getCode(), 0, 0);
+	}
+
+	@Override
+	public void onLoaded() {
+		for (EnumFacing facing : EnumFacing.VALUES) {
+			BlockPos sidedPos = pos.offset(facing);
+			if (world.isBlockLoaded(sidedPos)) {
+				world.neighborChanged(sidedPos, Blocks.AIR, pos);
+			}
+		}
+		if (world.isBlockLoaded(pos)) {
+			world.neighborChanged(pos, Blocks.AIR, pos);
+		}
+	}
+
+	@Override
+	public void onBlockUpdate(Block block) {
+		super.onBlockUpdate(block);
+		if (!this.isRendering()) {
+			RotationList newList = RotationList.EMPTY;
+			EnumFacing[] var3 = EnumFacing.VALUES;
+			int var4 = var3.length;
+			for (int var5 = 0; var5 < var4; ++var5) {
+				EnumFacing dir = var3[var5];
+				TileEntity tile = this.getWorld().getTileEntity(this.getPos().offset(dir));
+				if (tile instanceof GTTilePipe) {
+					newList = newList.add(dir);
+				}
+			}
+			if (this.connection.getCode() != newList.getCode()) {
+				this.connection = newList;
+				this.getNetwork().updateTileEntityField(this, "connection");
+			}
+		}
+	}
 }
