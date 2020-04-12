dddd9d1db682f27c43c9b39d6432927fda01489f
e99999 <26906600+e99999@users.noreply.github.com>
2020-04-12 09:02:24 -0700

    added ability to invert lamps

--- src/main/java/gtclassic/common/tile/GTTileLamp.java
+++ src/main/java/gtclassic/common/tile/GTTileLamp.java
@@ -7,21 +7,28 @@ import gtclassic.api.material.GTMaterialGen;
 import gtclassic.api.tile.GTTileBaseRecolorableTile;
 import gtclassic.common.GTBlocks;
 import ic2.api.classic.network.adv.NetworkField;
+import ic2.core.IC2;
 import ic2.core.util.misc.StackUtil;
+import ic2.core.util.obj.IClickable;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ITickable;
 import net.minecraft.world.EnumSkyBlock;
+import net.minecraftforge.fml.relauncher.Side;
 
-public class GTTileLamp extends GTTileBaseRecolorableTile implements ITickable {
+public class GTTileLamp extends GTTileBaseRecolorableTile implements ITickable, IClickable {
 
 	@NetworkField(index = 10)
 	public int redstoneLevel = -1;
+	private boolean inverted;
 	private boolean shouldUpdate;
 	private static final String NBT_LEVEL = "redstoneLevel";
+	private static final String NBT_INVERT = "inverted";
 
 	public GTTileLamp() {
 		super(0);
@@ -39,6 +46,19 @@ public class GTTileLamp extends GTTileBaseRecolorableTile implements ITickable {
 	}
 
 	@Override
+	public void readFromNBT(NBTTagCompound nbt) {
+		super.readFromNBT(nbt);
+		this.inverted = nbt.getBoolean(NBT_INVERT);
+	}
+
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
+		super.writeToNBT(nbt);
+		nbt.setBoolean(NBT_INVERT, this.inverted);
+		return nbt;
+	}
+
+	@Override
 	public void onBlockUpdate(Block block) {
 		this.shouldUpdate = true;
 	}
@@ -47,7 +67,8 @@ public class GTTileLamp extends GTTileBaseRecolorableTile implements ITickable {
 	public void update() {
 		if (this.shouldUpdate || this.redstoneLevel == -1) {
 			int oldlevel = this.redstoneLevel;
-			this.redstoneLevel = this.getHighestRedstoneLevel();
+			int highLevel = this.getHighestRedstoneLevel();
+			this.redstoneLevel = this.inverted ? 15 - highLevel : highLevel;
 			this.setActive(this.redstoneLevel > 0);
 			if (oldlevel != this.redstoneLevel) {
 				this.getNetwork().updateTileEntityField(this, NBT_LEVEL);
@@ -91,5 +112,35 @@ public class GTTileLamp extends GTTileBaseRecolorableTile implements ITickable {
 	@Override
 	public List<ItemStack> getInventoryDrops() {
 		return getDrops();
+	}
+
+	@Override
+	public boolean hasLeftClick() {
+		return false;
+	}
+
+	@Override
+	public boolean hasRightClick() {
+		return true;
+	}
+
+	@Override
+	public void onLeftClick(EntityPlayer arg0, Side arg1) {
+		// Unused but needed for interface
+	}
+
+	@Override
+	public boolean onRightClick(EntityPlayer player, EnumHand hand, EnumFacing enumFacing, Side side) {
+		if (player.isSneaking() && player.getHeldItemMainhand().isEmpty()) {
+			this.inverted = !this.inverted;
+			this.shouldUpdate = true;
+			if (this.isSimulating()) {
+				String msg = this.inverted ? "Inverted" : "Not Inverted";
+				IC2.platform.messagePlayer(player, msg);
+			}
+			player.playSound(SoundEvents.UI_BUTTON_CLICK, 1.0F, 1.0F);
+			return true;
+		}
+		return false;
 	}
 }
